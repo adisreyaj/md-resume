ce7410017b38dbebc188bed2cd0a3ca706f9dd90
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-09-19 14:13:42 +0530

    feat: add split section

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { markdown } from '@codemirror/lang-markdown';
 import { oneDark } from '@codemirror/theme-one-dark';
 import { EditorView, ViewUpdate } from '@codemirror/view';
 import debounce from 'just-debounce';
+import Split from 'split.js';
 import './main.scss';
 import Worker from './workers/markdown.worker?worker';
 
@@ -10,8 +11,7 @@ const worker = new Worker();
 const markdownInput = document.getElementById(
   'markdown'
 ) as HTMLTextAreaElement;
-const markdownContainer = document.querySelector('.app__markdown');
-setEditorWidth();
+Split(['.app__markdown', '.app__rendered']);
 initEditor(markdownInput);
 const rendered = document.getElementById('rendered') as HTMLDivElement;
 worker?.addEventListener('message', ({ data }) => {
@@ -44,14 +44,4 @@ function debouncedRender(markdown: string) {
 
 function render(markdown: string) {
   worker.postMessage({ markdown });
-}
-
-function setEditorWidth() {
-  if (markdownInput) {
-    markdownInput.style.maxWidth = `${markdownContainer?.clientWidth}px`;
-  }
-  window.addEventListener('resize', () => {
-    console.log('resizing');
-    markdownInput.style.maxWidth = `${markdownContainer?.clientWidth}px`;
-  });
 }
